06d7bbef69a2b1a3c8d49ffd2d743c2def2297bd
Khristian Rojas <khristianrojas15@gmail.com>
2021-04-07 18:34:11 -0400

    Add contact section

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import About from "./components/About";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import Contact from "./components/Contact";
 
 class App extends Component {
 
@@ -131,6 +132,10 @@ class App extends Component {
         />
         <Experience
           resumeExperience={this.state.resumeData.experience}
+          resumeBasicInfo={this.state.resumeData.basic_info}
+        />
+        <Contact
+          sharedContacts={this.state.sharedData.contacts}
           resumeBasicInfo={this.state.resumeData.basic_info}
         />
         <Footer sharedBasicInfo={this.state.sharedData.basic_info} />
